d5860d9478ea19d3982e48c9f894cea75faf0aba
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2025-03-25 09:03:00 +0100

    Hammerspoon: Open Wezterm on external display/screen when available

--- Hammerspoon/init.lua
+++ Hammerspoon/init.lua
@@ -1,3 +1,9 @@
+function postitionQuakeTerminal(window)
+    -- Position and size window
+    window:setTopLeft(hs.geometry.point(400,0))
+    window:setSize(hs.geometry.size(800,400))
+end
+
 
 -- "Quake" terminal for WezTerm
 -- From: https://github.com/kovidgoyal/kitty/issues/45#issuecomment-568920629
@@ -17,10 +23,24 @@ hs.hotkey.bind({"ctrl"}, "§", function()
         app = hs.application.get("WezTerm")
     end
 
-  -- app:mainWindow():moveToUnit'[0.0,0.0,0.5,0.5]'
-  -- app:mainWindow():moveToUnit'[0.33,0.33,0.66,0.66]'
-  app:mainWindow():setTopLeft(hs.geometry.point(400,0))
-  app:mainWindow():setSize(hs.geometry.size(800,400))
+    local window = app:mainWindow()
+
+    -- If we have more than 1 screen put the terminal on the large screen (assumed to be `screens[2]`)
+    -- You'd think you could just run `positionQuakeTerminal()` before the below code and do away with
+    -- the `else`-branch. But that produces a lot of flashing of the terminal window, whereas this
+    -- structure of the code does not.
+    local screens = hs.screen.allScreens()
+    if #screens > 1 then
+        local currentScreen = window:screen()
+        local targetScreen = screens[2]
+
+        if currentScreen ~= targetScreen then
+            postitionQuakeTerminal(window)
+            window:moveToScreen(targetScreen, false, true, 0)
+        end
+    else
+        postitionQuakeTerminal(window)
+    end
 end)
 
 hs.loadSpoon("MalthesMoveWindow")
