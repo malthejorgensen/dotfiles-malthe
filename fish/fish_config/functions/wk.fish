da4c7ee0c442a0a291e4fe6dfbb0a885302bbd32
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2026-03-18 11:07:23 +0100

    wk: Add `wk delete` to delete the worktree

--- fish/fish_config/functions/wk.fish
+++ fish/fish_config/functions/wk.fish
@@ -1,4 +1,9 @@
 function wk
-    # `tee /dev/stderr` mirrors output to terminal while still capturing it; `[-1]` grabs the last line (the worktree path)
-    set wt (wk_impl $argv | tee /dev/stderr)[-1] && cd $wt
+    if test (count $argv) -ge 1 -a "$argv[1]" = delete
+        # `tee /dev/stderr` mirrors output to terminal while still capturing it; `[-1]` grabs the last line (the repo root)
+        set repo_root (wk_impl delete | tee /dev/stderr)[-1] && cd $repo_root
+    else
+        # `tee /dev/stderr` mirrors output to terminal while still capturing it; `[-1]` grabs the last line (the worktree path)
+        set wt (wk_impl $argv | tee /dev/stderr)[-1] && cd $wt
+    end
 end
